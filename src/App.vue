411984a69da404876718ec0fc9bdeb1a872a4d62
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-04-15 14:07:15 +0200

    create scaffolding and set App.vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        title: 'Hello wordl!'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
